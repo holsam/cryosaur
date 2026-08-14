ffcec274cf999539373fbd8bcc34e45bf616bdb5
Sam Holland <hol_sam@icloud.com>
2026-08-14 12:14:46 +0100

    feat(cli): add callback to configure logging
    - Modified cli.py to add callback with options for configuring logging.

--- src/cryosaur/cli.py
+++ src/cryosaur/cli.py
@@ -4,6 +4,11 @@ CRYOSAUR: main CLI entrypoint
 
 # -- Import external dependencies
 import typer
+from pathlib import Path
+from typing import Annotated, Literal
+
+# -- Import cryosaur utilities
+from cryosaur.utils.log import configure_logging, log
 
 # -- Initialise Typer class for cryosaur CLI
 cryosaur = typer.Typer(
@@ -11,3 +16,31 @@ cryosaur = typer.Typer(
     rich_markup_mode='rich',
     no_args_is_help=True,
 )
+
+# -- Create callback to provide logging options and configure logging
+@cryosaur.callback()
+def logging_callback(
+    log_dir: Annotated[
+        Path | None,
+        typer.Option('-d', '--directory', help='Directory to write log file to.', show_default=None, rich_help_panel='Logging Options')
+    ] = None,
+    log_mode: Annotated[
+        Literal['append', 'new', 'overwrite'],
+        typer.Option('-m', '--mode', help='Mode to use for resolving log file.', rich_help_panel='Logging Options')
+    ] = 'append',
+    quiet: Annotated[
+        bool,
+        typer.Option('-q', '--quiet', help='Disable all logging except errors.', show_default=False, rich_help_panel='Logging Options')
+    ] = False,
+    verbosity: Annotated[
+        int,
+        typer.Option('-v', '--verbose', max=3, clamp=True, count=True, help='Increase verbosity of logging.', show_default=False, rich_help_panel='Logging Options')
+    ] = 0,
+):
+    # Check quiet and verbose haven't been supplied together
+    if quiet and verbosity:
+        raise typer.BadParameter('-q/--quiet and -v/--verbose are incompatible')
+    # Configure logging
+    log_path = configure_logging(directory=log_dir, mode=log_mode, quiet=quiet, verbosity=verbosity)
+    # Output confirmation message that logging has been set up
+    log.info(f'Log messages will be {"appended" if log_mode == 'append' else "written"} to <cyan>{log_path}</cyan>')
