ff402f5ce514befa0005534d91997c9daadc180c
Sam Holland <hol_sam@icloud.com>
2026-08-17 13:44:32 +0100

    feat(cli): register `trim-vol` command with cli
    - Modified cryosaur/utils/cli/__init__.py to register `trim-vol` command.

--- src/cryosaur/utils/cli/__init__.py
+++ src/cryosaur/utils/cli/__init__.py
@@ -4,5 +4,6 @@ CRYOSAUR: commands module
 
 # -- Import cryosaur commands
 from cryosaur.commands.destripe_lamella import cli as _destripe_lamella_cli
+from cryosaur.commands.trim_vol import cli as _trim_volume_cli
 from cryosaur.commands.internal import resolve_star_paths as _resolve_star_path_cli
 from cryosaur.commands.utils import flatten as _flatten_cli
