a28865d1c4c60eca674c42c353510460214326f9
Sam Holland <hol_sam@icloud.com>
2026-08-18 13:48:45 +0100

    feat(utils): add `utils check-tools` command
    - Added cryosaur/commands/utils/check_external_tools.py to define new
    command `utils check-tools` to ensure all required external tools are
    available on PATH for a command.
    - Modified cryosaur/utils/cli/__init__.py to register new command.

--- src/cryosaur/utils/cli/__init__.py
+++ src/cryosaur/utils/cli/__init__.py
@@ -6,4 +6,5 @@ CRYOSAUR: commands module
 from cryosaur.commands.destripe_lamella import cli as _destripe_lamella_cli
 from cryosaur.commands.trim_vol import cli as _trim_volume_cli
 from cryosaur.commands.internal import resolve_star_paths as _resolve_star_path_cli
+from cryosaur.commands.utils import check_external_tools as _check_tools_cli
 from cryosaur.commands.utils import flatten as _flatten_cli
