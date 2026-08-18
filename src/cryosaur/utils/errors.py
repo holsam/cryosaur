2f46be02cd4756fc79b91d9aac086012e059ca50
Sam Holland <hol_sam@icloud.com>
2026-08-18 18:40:44 +0100

    feat(cli): log completion on exit
    - Modified cryosaur/utils/errors.py to log a completion message with
    exit code when cryosaur either completes execution or raises a
    CryosaurError.

--- src/cryosaur/utils/errors.py
+++ src/cryosaur/utils/errors.py
@@ -20,8 +20,13 @@ def handle_errors(func: Callable) -> Callable:
     @wraps(func)
     def wrapper(*args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            result = func(*args, **kwargs)
         except CryosaurError as exc:
             log.error(str(exc))
+            log.info('cryosaur completed with exit code 1')
+            print()
             raise typer.Exit(code=1)
+        log.info('cryosaur completed with exit code 0')
+        print()
+        return result
     return wrapper
