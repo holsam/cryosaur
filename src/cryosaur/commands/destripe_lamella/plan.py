cb9e0345db9961bb6fe394767bcd8479a2b96b4d
Sam Holland <hol_sam@icloud.com>
2026-08-18 14:51:42 +0100

    fix(destripe_lamella): fix incorrect command
    - Modified cryosaur/commands/destripe_lamella/plan.py to fix incorrect
    command name used in RELION steps.

--- src/cryosaur/commands/destripe_lamella/plan.py
+++ src/cryosaur/commands/destripe_lamella/plan.py
@@ -70,7 +70,7 @@ def _stage_relion_job(
     staged_path = fork_dir / 'cryosaur' / 'staged' / f'{step_name}.star'
     write_job_star(relion_job_type, options, staged_path, is_tomo=is_tomo)
     return [
-        f'cryosaur internal resolve-star-path --fork-dir {fork_dir} --job-star {staged_path}',
+        f'cryosaur internal resolve-star-paths --fork-dir {fork_dir} --job-star {staged_path}',
         f'cd {fork_dir} && pipeliner --run_job {staged_path.relative_to(fork_dir)}',
     ]
 
